5b1c61ba5c61cad12d36909fd413dcbb3e23c4ca
Ian Handel <ian.handel@ed.ac.uk>
2021-02-07 16:00:36 +0000

    tweak map and tidy code

--- dogwalkr.R
+++ dogwalkr.R
@@ -2,14 +2,18 @@ library(tidyverse)
 library(sf)
 library(leaflet)
 
+meters_in_mile <- 1609
+
 ca <- read_sf("Local_Authority_Districts__December_2017__Boundaries_GB_BUC-shp/Local_Authority_Districts__December_2017__Boundaries_GB_BUC.shp")
 
 st_crs(ca)
 
 ca %>% 
-  st_buffer(dist = 8000) %>% 
+  st_buffer(dist = 5 * meters_in_mile) %>% 
   filter(LAD17NM %in% c("Scottish Borders", "City of Edinburgh")) %>%
   st_transform(ca1, crs = "+proj=latlon +datum=WGS84") %>% 
   leaflet() %>% 
   addTiles() %>% 
-  addPolygons()
+  addPolygons() %>% 
+  addMeasure() %>% 
+  addScaleBar()
